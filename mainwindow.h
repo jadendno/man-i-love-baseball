fc41abc728e18840a861e44b713c509ccf74391d
ScottyPhamboy <62496611+ScottyPhamboy@users.noreply.github.com>
2025-05-14 07:45:50 -0700

    /versions

---
+++ mainwindow.h
@@ -0,0 +1,42 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QMessageBox>
+#include <QGraphicsScene>
+#include <QGraphicsPixmapItem>
+#include <QGraphicsEllipseItem>
+#include <QObject>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    QGraphicsScene* scene;
+
+    void handleLogin();
+    void goToUsersMenu();
+    void goToEditMenu();
+    void logoutToLogin();
+    void goToAdminsMenu();
+
+    void clearLogin();
+
+
+    //map setup
+    void setupMap();
+
+};
+#endif // MAINWINDOW_H
